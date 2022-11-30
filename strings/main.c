f98949a29a541a909916b30e95ab445a0fef0cc2
AtilaD <atila8d@gmail.com>
2022-11-30 17:09:44 -0300

    stringsV2

--- strings/main.c
+++ strings/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+#define O 20
+#define D 20
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	setlocale(LC_ALL,"Portuguese");
 	
+	char origem[O] = {"Olá mundo!\n"};
+	char destino[D] = {"Tudo bem com voces?\n\n"};
+	char origem2[O] = {"Origem2"};
+	char destino2[D] = {"Destino2"};
 	char c[10];
+	int i;
+	char hardText[O] = {"/exit"};
+	char senhaUsuario[O];
+	int ok;
 	
 	printf("Digite um dado: \n");
 	scanf("%s", c);
@@ -23,9 +37,42 @@ int main(int argc, char *argv[]) {
 	printf("Dado pelo fgets e puts: ");
 	puts(c);
 	
-	printf("Digite um dado: (gets) \n ");
+	printf("Digite um dado: (gets)\n");
 	gets(c);
 	fflush(stdin);
 	puts(c);
+	printf("\n \n");
+	
+	printf("Antes do strcpy: \n");
+	puts(origem);
+	puts(destino);
+	
+	strcpy(destino, origem);
+	
+	printf("depois do strcpy: \n");
+	puts(origem);
+	puts(destino);
+	printf("\n\n");
+	
+	strcat(destino2, origem2);
+	
+	printf("Agora strcat para origem e destino 2 \n");
+	puts(destino2);
+	printf("\n\n");
+	
+	printf("Tamanho do texto acima é: ");
+	i = strlen(destino2);
+	printf("%d \n\n", i);
+	
+	printf("Digite sua senha: \n");
+	gets(senhaUsuario);
+	
+	ok = strcmp(hardText, senhaUsuario);
+	
+	if(ok == 0){
+		printf("Senha esta correta \n");
+	}else{
+		printf("Senha errada! \n");
+	}
 	return 0;
 }
